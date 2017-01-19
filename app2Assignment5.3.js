7128eff7addefb40d3ceb97a4366cf655f799001
Monicadorthy <monicadorthy@gmail.com>
2017-01-19 12:24:55 +0530

    Fifth GitHub Commit to master
    Fifth GitHub Commit to master

---
+++ app2Assignment5.3.js
@@ -0,0 +1,7 @@
+var Employee={name:"Employee name",
+age:"age", salary:"salary",address:"address",Address:{
+city:"city",state:"state",pincode:"pincode"
+}
+};
+console.log(Employee);
+console.log(Employee.Address);
