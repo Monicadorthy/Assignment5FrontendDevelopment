0ae65edbb07d48ce3cb36d50db7834a2cbe482cc
Monicadorthy <monicadorthy@gmail.com>
2017-01-19 11:52:42 +0530

    Fifth GitHub Commit
    Fifth GitHub Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+var name="Name";
+var age="Age";
+var dateofbirth="Date Of Birth";
+var placeofbirth="Place Of Birth";
+
+myfunction();
+function myfunction(){
+    console.log(name);
+    console.log(age);
+    console.log(dateofbirth);
+    console.log(placeofbirth);
+
+}
+
