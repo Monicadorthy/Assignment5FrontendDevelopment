11b146209d2c74428677f836191df59502bf6808
Monicadorthy <monicadorthy@gmail.com>
2017-01-19 12:27:25 +0530

    Fifth GitHub 4th Assignment
    Fifth GitHub 4th Assignment

---
+++ app3Assignment5.4.js
@@ -0,0 +1,10 @@
+
+var Employee= ["Employee name","age","salary","city","state","pincode",
+ {Employeename1:"Employee name" ,age:"age", salary:"salary" ,city:"city ",state:"state", pincode:"pincode"}
+,{Employeename2:"Employee name" ,age:"age", salary:"salary" ,city:"city ",state:"state", pincode:"pincode"}
+,{Employeename3:"Employee name" ,age:"age", salary:"salary" ,city:"city ",state:"state", pincode:"pincode"}
+,{Employeename4:"Employee name" ,age:"age", salary:"salary" ,city:"city ",state:"state", pincode:"pincode"}
+,{Employeename5:"Employee name" ,age:"age", salary:"salary" ,city:"city ",state:"state", pincode:"pincode"}
+
+];
+console.log(Employee);
